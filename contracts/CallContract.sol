c5a85f2c8920ad71bdb68f9d5dd1e396bd436715
Stephanie Zhang <stevie.zhang@mail.utoronto.ca>
2019-08-08 09:22:30 -0400

    cleaned up comments

--- contracts/CallContract.sol
+++ contracts/CallContract.sol
@@ -7,7 +7,8 @@ contract CallContract {
     function callOtherContract(uint currentDepth, uint targetDepth) public returns (bool) {
         //implicitly true
         bool success = true;
-        bytes4 sig = bytes4(keccak256("callOtherContract(uint,uint)")); //Function signature
+        //Function signature
+        bytes4 sig = bytes4(keccak256("callOtherContract(uint,uint)"));
         uint nextDepth = currentDepth++;
         address calleeAddr = calleeAddress;
         if (currentDepth < targetDepth) {
@@ -15,15 +16,15 @@ contract CallContract {
             uint g = gasleft();
             assembly{
 
-                let ptr := mload(0x40)          //Find empty storage location using "free memory pointer"
-			    mstore(ptr,sig)                 //Place signature at begining of empty storage
-                mstore(add(ptr,0x04),nextDepth) //First uint parameter, add 4 bytes just after signature ptr start location
-                mstore(add(ptr,0x24),targetDepth)         //2nd uint parameter placed right after the first parameter, add 36 bytes after the signature ptr start location
-			    mstore(0x40,add(ptr,0x64))      //Reset free pointer before the function call
+                let ptr := mload(0x40)               //Find empty storage location using "free memory pointer"
+			    mstore(ptr,sig)                     //Place signature at begining of empty storage
+                mstore(add(ptr,0x04),nextDepth)     //First uint parameter, add 4 bytes just after signature ptr start location
+                mstore(add(ptr,0x24),targetDepth)   //2nd uint parameter placed right after the first parameter, add 36 bytes after the signature ptr start location
+			    mstore(0x40,add(ptr,0x64))          //Reset free pointer before the function call
 
 			    let returnValue := call(
                                     g,              //gas left
-				                    calleeAddr,  //To address
+				                    calleeAddr,     //To address
 				                    0,              //value, we pass in none
 				                    ptr,            //inputs start at location ptr
 				                    0x44,           //size of inputs total, 68 bytes (4 (sig) + 32 (uint) + 32 (uint))
