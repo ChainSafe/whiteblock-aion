240c3e13d46ca077189e8b67a3e5a001f3f1c151
Stephanie <stevie.zhang@mail.utoronto.ca>
2019-08-09 11:06:46 -0400

    More detailed explanation on why this won't work

--- contracts/SimpleContractWithoutABI.sol
+++ contracts/SimpleContractWithoutABI.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.5.0;
 contract SimpleContractWithoutABI {
     // This will have an ABI. The closest thing to a no-ABI smart contract
     // is a contract containing only a constructor, and constructors can't
-    // return value in solidity.
+    // return value in solidity. This means that we would not be able to
+    // interact with an ABIless smart contract in a meaningful way like in
+    // the Aion contract we are trying to replicate.
     function main() public pure returns (bytes32) {
         return 0;
     }
