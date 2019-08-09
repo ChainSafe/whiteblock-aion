1030f1d7e5824851b95476f509768f070b75a3e8
Priom Chowdhury <priom.galaxy@gmail.com>
2019-08-09 13:06:40 -0400

    removed redundant abs

--- script2/nodeParser.py
+++ script2/nodeParser.py
@@ -11,7 +11,9 @@ import json
     - Once the parser goes through the list, the actual test run dataPoint are extracted using a2min and are
      stored into (after2Min)
 '''
-def aggerateData (p, tps):
+
+
+def aggerateData(p, tps):
     with open(p, 'r') as f:
         nodeInfo = json.load(f)
 
@@ -21,11 +23,10 @@ def aggerateData (p, tps):
         "blockSizes": [],
         "totalTransactions": [],
         "txThroughPut": [],
-        "timestamps" : [],
-        "blockTime" : [],
+        "timestamps": [],
+        "blockTime": [],
         "blockNumber": []
     }
-
 
     initialStamp = int(nodeInfo[0]['timestamp'], 0)
     previousStamp = int(nodeInfo[0]['timestamp'], 0)
@@ -41,36 +42,38 @@ def aggerateData (p, tps):
         blockSize = int(blockInfo['size'], 0)
         totalTransactions = len(blockInfo['transactions'])
         blockNumber = blockInfo['number']
-        
+
         if totalTransactions > 0 and not txCheck:
             txCheck, initialStamp = True, timeStamp
-            previousBlockIndex =blockNumber - 2
+            previousBlockIndex = blockNumber - 2
             previousStamp = int(nodeInfo[previousBlockIndex]['timestamp'], 0)
 
-
         if txCheck:
-            
+
             fullLength['blockSizes'].append(blockSize)
             fullLength["blockTime"].append(timeStamp - previousStamp)
             fullLength["timestamps"].append(timeStamp)
             fullLength["totalTransactions"].append(totalTransactions)
             fullLength["blockNumber"].append(blockNumber)
 
-            previousStamp = int(blockInfo['timestamp'],0)
+            previousStamp = int(blockInfo['timestamp'], 0)
             diff = (timeStamp - initialStamp)
 
             print("timeStamp", timeStamp)
             print("previousStamp", previousStamp)
             print("initialStamp", initialStamp)
             print("diff", diff)
-            
-            if (diff >= 120):
-                diff1 = abs(120 - diff)
-                diff2 = abs(120 - (timeStamp -  fullLength["timestamps"][-2]))
 
-                #initialStamp = Timestamp of the first blockWithTx
-                #diff1 = how close the current timeStamp is to the 120 mark from initialStamp
-                #diff2 = how close is the timeStamp of the pervious block to the 120 mark from initialStamp
+            if (diff >= 120):
+                diff1 = diff - 120
+                print("diff1", diff1)
+
+                diff2 = (timeStamp - fullLength["timestamps"][-2]) - 120
+                print("diff2", diff2)
+
+                # initialStamp = Timestamp of the first blockWithTx
+                # diff1 = how close the current timeStamp is to the 120 mark from initialStamp
+                # diff2 = how close is the timeStamp of the pervious block to the 120 mark from initialStamp
 
                 if startStamp == 0:
                     a2min = index
@@ -79,28 +82,26 @@ def aggerateData (p, tps):
                     if diff2 < diff1:
                         a2min = index - 1
             index += 1
-    
-    startIndex = fullLength["blockNumber"][a2min] -1
+
+    startIndex = fullLength["blockNumber"][a2min] - 1
     intervalStart = int(nodeInfo[startIndex]['timestamp'], 0)
     intervalEnd = int(nodeInfo[-1]['timestamp'], 0)
 
     txSent = tps * (intervalEnd - intervalStart)
-    
+
     after2Min = {
         "blockSizes": fullLength['blockSizes'][a2min:],
         "totalTransactions": fullLength['totalTransactions'][a2min:],
-        "timestamps" : fullLength['timestamps'][a2min:],
-        "blockTime" : fullLength['blockTime'][a2min:],
+        "timestamps": fullLength['timestamps'][a2min:],
+        "blockTime": fullLength['blockTime'][a2min:],
         "txSent": txSent
     }
-    
-    
-    after2Min['avgBlockSize'] = sum(after2Min['blockSizes'])/len(after2Min["blockSizes"])
-    after2Min['avgBlockTime'] = sum(after2Min['blockTime'])/len(after2Min["blockTime"])
-    after2Min['txSuccessRate'] = sum(after2Min['totalTransactions'])/txSent
-    after2Min['avgTxThroughPut'] = sum(after2Min['totalTransactions'])/(intervalEnd - intervalStart)
-    after2Min['TimeStampOfStartBlock'] = intervalStart
 
+    after2Min['avgBlockSize'] = sum(after2Min['blockSizes']) / len(after2Min["blockSizes"])
+    after2Min['avgBlockTime'] = sum(after2Min['blockTime']) / len(after2Min["blockTime"])
+    after2Min['txSuccessRate'] = sum(after2Min['totalTransactions']) / txSent
+    after2Min['avgTxThroughPut'] = sum(after2Min['totalTransactions']) / (intervalEnd - intervalStart)
+    after2Min['TimeStampOfStartBlock'] = intervalStart
 
     # print("Avg block size", after2Min['avgBlockSize'], "\n" )
     # print("Avg Block time", after2Min['avgBlockTime'], "seconds \n")
